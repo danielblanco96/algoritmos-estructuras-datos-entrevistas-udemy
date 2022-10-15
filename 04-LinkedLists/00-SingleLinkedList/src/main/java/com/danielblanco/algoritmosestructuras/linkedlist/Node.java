a49cb81de427ed200781fe36726a8ed2cc68210a
Daniel Blanco <dablancal96@gmail.com>
2022-10-15 10:04:35 +0100

    Add single linked list example

---
+++ 04-LinkedLists/00-SingleLinkedList/src/main/java/com/danielblanco/algoritmosestructuras/linkedlist/Node.java
@@ -0,0 +1,10 @@
+package com.danielblanco.algoritmosestructuras.linkedlist;
+
+public class Node {
+	public Node next;
+	public int value;
+
+	public Node(int value) {
+		this.value = value;
+	}
+}
---
+++ 04-LinkedLists/00-SingleLinkedList/src/main/java/com/danielblanco/algoritmosestructuras/linkedlist/SingleLinkedList.java
@@ -0,0 +1,50 @@
+package com.danielblanco.algoritmosestructuras.linkedlist;
+
+public class SingleLinkedList {
+	public Node head;
+
+	public void appentToTail(int value) {
+		if (head == null) {
+			head = new Node(value);
+			return;
+		}
+
+		Node current = head;
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = new Node(value);
+	}
+
+	public void deleteNode(int value) {
+		if (head == null)
+			return;
+
+		if (head.value == value) {
+			head = head.next;
+			return;
+		}
+
+		Node n = head;
+		while (n.next != null) {
+			if (n.next.value == value) {
+				n.next = n.next.next;
+				return;
+			}
+			n = n.next;
+		}
+	}
+
+	public void print() {
+		if (head == null) {
+			System.out.println("END");
+			return;
+		}
+		Node current = head;
+		while (current.next != null) {
+			System.out.print(current.value + " -> ");
+			current = current.next;
+		}
+		System.out.println(current.value + " -> END");
+	}
+}
