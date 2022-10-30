5cf66d2b57b1a7820c9c7a849d43cbd1901314f0
Daniel Blanco <dablancal96@gmail.com>
2022-10-30 19:58:34 +0000

    Refactor binary search iterative

--- 11-Searching/00-BinarySearch/src/main/java/com/danielblanco/algoritmosestructuras/searching/BinarySearchIterative.java
+++ 11-Searching/00-BinarySearch/src/main/java/com/danielblanco/algoritmosestructuras/searching/BinarySearchIterative.java
@@ -3,22 +3,20 @@ package com.danielblanco.algoritmosestructuras.searching;
 public class BinarySearchIterative {
 
   int binarySearch(int[] array, int target) {
-    int index = -1;
     int left = 0;
     int right = array.length - 1;
 
     while (left <= right) {
       int mid = left + ((right - left) / 2);
+
+      if (array[mid] == target) return mid;
       if (array[mid] < target) {
         left = mid + 1;
-      } else if (array[mid] > target) {
+      } else {
         right = mid - 1;
-      } else if (array[mid] == target) {
-        index = mid;
-        break;
       }
     }
 
-    return index;
+    return -1;
   }
 }
