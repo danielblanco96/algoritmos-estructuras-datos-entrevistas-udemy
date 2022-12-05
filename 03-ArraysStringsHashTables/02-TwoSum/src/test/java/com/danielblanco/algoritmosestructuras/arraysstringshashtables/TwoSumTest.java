0e604b453ebadf636034282fd8ce9d164a0de7c5
Daniel Blanco <dablancal96@gmail.com>
2022-12-05 20:01:31 +0000

    TwoSum test

--- 03-ArraysStringsHashTables/02-TwoSum/src/test/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/TwoSumTest.java
+++ 03-ArraysStringsHashTables/02-TwoSum/src/test/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/TwoSumTest.java
@@ -10,7 +10,7 @@ public class TwoSumTest {
   @Test
   public void isUniqueTest() {
     int[] array = new int[] {9, 2, 5, 6};
-    TwoSumSolution twoSum = new TwoSumSolution();
+    TwoSum twoSum = new TwoSum();
     int[] result = twoSum.twoSum(array, 7);
     assertTrue((result[0] == 1 && result[1] == 2) || (result[0] == 2 && result[1] == 1));
     assertEquals(null, twoSum.twoSum(array, 50));
