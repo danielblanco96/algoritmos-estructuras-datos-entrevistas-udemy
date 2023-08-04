7736fbbee87b1f45cde5f67f88710a4f64c235b3
zidk <zidesk@gmail.com>
2023-08-04 12:05:49 +0200

    Ejemplo incorrecto y clase con contenido

--- 04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_04_add_two_numbers/AddTwoNumbers.java
+++ 04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_04_add_two_numbers/AddTwoNumbers.java
@@ -7,42 +7,13 @@ import com.danielblanco.algoritmosestructuras._00_linkedlist.Node;
  * dos enteros positivos. Las listas están en posición invertida.
  *
  * Ejemplo:
- *  Input: 1->2->4, 5->2->8
- *  Output: 6->4->2->1
+ *  Input: 1->2->4->6, 5->2->8
+ *  Output: 6->4->2->7
  *  6421 + 825 = 7246
  */
 public class AddTwoNumbers {
 
   public Node addTwoNumbers(Node list1, Node list2) {
-    Node result = new Node(-1);
-    Node current = result;
-    int carry = 0;
-
-    while (list1 != null || list2 != null) {
-      current.next = new Node(-1);
-      current = current.next;
-      int digit = carry;
-
-      if (list1 != null) {
-        digit += list1.value;
-        list1 = list1.next;
-      }
-
-      if (list2 != null) {
-        digit += list2.value;
-        list2 = list2.next;
-      }
-
-      carry = digit / 10;
-      digit = digit % 10;
-
-      current.value = digit;
-    }
-
-    if (carry > 0) {
-      current.next = new Node(carry);
-    }
-
-    return result.next;
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 }
